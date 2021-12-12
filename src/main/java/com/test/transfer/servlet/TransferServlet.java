a967fd4b5ae22b348d203b4ea070dc3b34ec8308
tangyw <tangyw@landray.com.cn>
2021-12-12 19:52:49 +0800

    传统开发模式问题分析

---
+++ src/main/java/com/test/transfer/servlet/TransferServlet.java
@@ -0,0 +1,51 @@
+package com.test.transfer.servlet;
+
+import com.test.transfer.pojo.Result;
+import com.test.transfer.service.TransferService;
+import com.test.transfer.service.impl.TransferServiceImpl;
+import com.test.transfer.utils.JsonUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "TransferServlet", value = "/TransferServlet")
+public class TransferServlet extends HttpServlet {
+
+    // 1. 实例化service层对象
+    private TransferService transferService = new TransferServiceImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 设置请求体的字符编码
+        req.setCharacterEncoding("UTF-8");
+
+        String fromCardNo = req.getParameter("fromCardNo");
+        String toCardNo = req.getParameter("toCardNo");
+        String moneyStr = req.getParameter("money");
+        int money = Integer.parseInt(moneyStr);
+
+        Result result = new Result();
+
+        try {
+
+            // 2. 调用service层方法
+            transferService.transfer(fromCardNo,toCardNo,money);
+            result.setStatus("200");
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.setStatus("201");
+            result.setMessage(e.toString());
+        }
+
+        // 响应
+        resp.setContentType("application/json;charset=utf-8");
+        resp.getWriter().print(JsonUtils.object2Json(result));
+    }
+}
